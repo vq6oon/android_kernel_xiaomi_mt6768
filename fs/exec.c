fbcf3ee3a364c763c1ef6015e9cd2455850fbc64
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-12-14 10:14:04 +0800

    [BACKPORT] fs: Implement SuSFS v2.0.0
    * sus_mount and others are complied with the upstream susfs kernel branch v5.10
    * susfs try_umount is restored
    * auto features are deprecated so you have to hide your mounts manually
    
    This susfs backport commit requires a set of backports to comply with the upstream susfs:
    d406904bb175cb9791b5aed4404df3268859d91b fs: Convert namespace IDAs to new API
    9ba899398576d329288b023566d3a731470ad13a fs: Convert unnamed_dev_ida to new API
    e784224542fab4b3888d9103a1f678015221a7d1 ida: Add new API
    ed8fd10d97a50ec424ae38a40351b5de060f9838 ida: Lock the IDA in ida_destroy
    4ba6dccf73a620e565803e5995d5748ecfbb56a2 lib/idr.c: remove simple_ida_loc
    0a8cbf3 proc: less memory for /proc/*/map_files readdir
    37ae244 mm: smaps: split PSS into components
    49a5115 fs/task_mmu: add pkeys header
    6f94042 mm/pkeys: Add an empty arch_pkeys_enabled()
    bbd5aec mm/pkeys, powerpc, x86: Provide an empty vma_pkey() in linux/pkeys.h
    849ca8c mm: /proc/pid/smaps_rollup: convert to single value seq_file
    6071a48 mm: /proc/pid/smaps: factor out common stats printing
    03fd2fb mm: /proc/pid/smaps: factor out mem stats gathering
    95f8be4 mm: /proc/pid/*maps remove is_pid and related wrappers
    27956d2 procfs: add seq_put_hex_ll to speed up /proc/pid/maps
    7f2847d proc: add seq_put_decimal_ull_width to speed up /proc/pid/smaps
    
    For KernelSU, Use my RKSU fork which it includes susfs try_umount:
    https://github.com/sidex15/KernelSU/tree/susfs-rksu-master
    
    Or if you want to patch it within RKSU, use this commit:
    https://github.com/sidex15/KernelSU/commit/8be13a387a7b32f6c70b9077a97c30840254f630

--- fs/exec.c
+++ fs/exec.c
@@ -62,6 +62,9 @@
 #include <linux/oom.h>
 #include <linux/compat.h>
 #include <linux/vmalloc.h>
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs_def.h>
+#endif
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
@@ -1716,6 +1719,17 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+
+#ifdef CONFIG_KSU_SUSFS
+extern bool ksu_execveat_hook __read_mostly;
+extern bool susfs_is_boot_completed_triggered __read_mostly;
+extern bool __ksu_is_allow_uid_for_current(uid_t uid);
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
+				void *envp, int *flags);
+#endif
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1730,6 +1744,20 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
+#ifdef CONFIG_KSU_SUSFS
+	if (likely(susfs_is_current_proc_umounted())) {
+		goto orig_flow;
+	}
+
+	if (unlikely(ksu_execveat_hook || !susfs_is_boot_completed_triggered)) {
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	}
+
+orig_flow:
+#endif
+	
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
 	 * set*uid() to execve() because too many poorly written programs
@@ -1878,7 +1906,7 @@ out_ret:
 	return retval;
 }
 
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_SUSFS)
 __attribute__((hot))
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
 				void *argv, void *envp, int *flags);
@@ -1890,7 +1918,7 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_SUSFS)
 	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
 #endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
@@ -1920,7 +1948,7 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
-#ifdef CONFIG_KSU // 32-bit ksud and 32-on-64 support
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_SUSFS) // 32-bit ksud and 32-on-64 support
 	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
 #endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
